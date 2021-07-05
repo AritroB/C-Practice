2ea7cfb871a0fbd2ace70917e1f9668e1c27b790
AritroB <abasuusa2010@gmail.com>
2021-07-05 15:00:44 -0500

    Added define statements

--- forcftable.c
+++ forcftable.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
 
 /* print Fahrenheit-Celsius table */
 
@@ -6,6 +9,6 @@ int main()
 {
     int fahr;
 
-    for (fahr = 0; fahr <= 300; fahr = fahr + 20)
+    for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
         printf("%3d %6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
 }
